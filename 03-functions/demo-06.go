818e22943a340dbc87eb983d605ddf984e52e0d1
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-13 16:45:52 +0530

    function composition using higher order functions

---
+++ 03-functions/demo-06.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	add := getLoggedOp(add)
+	subtract := getLoggedOp(subtract)
+	add(100, 200)
+	subtract(100, 200)
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func subtract(x, y int) int {
+	return x - y
+}
+
+func getLoggedOp(oper func(int, int) int) func(int, int) {
+	return func(n1, n2 int) {
+		fmt.Println("Before invocation")
+		result := oper(n1, n2)
+		fmt.Println("Result = ", result)
+		fmt.Println("After invocation")
+	}
+}
