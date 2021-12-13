59e1ab1803112676660b43ac3869488016fb4bb6
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-13 17:07:43 +0530

    closures - demo

--- 03-functions/demo-07.go
+++ 03-functions/demo-07.go
@@ -2,21 +2,30 @@ package main
 
 import "fmt"
 
-var counter int
-
 func main() {
+	increment, decrement := getCounters()
 	fmt.Println(increment()) //=> 1
 	fmt.Println(increment()) //=> 2
-	doSomething()
 	fmt.Println(increment()) //=> 3
 	fmt.Println(increment()) //=> 4
+	fmt.Println(decrement())
+	fmt.Println(decrement())
+	fmt.Println(decrement())
+	fmt.Println(decrement())
+	fmt.Println(decrement())
+	fmt.Println(decrement())
 }
 
-func increment() int {
-	counter++
-	return counter
-}
+func getCounters() (func() int, func() int) {
+	var counter int
+	increment := func() int {
+		counter++
+		return counter
+	}
+	decrement := func() int {
+		counter--
+		return counter
+	}
+	return increment, decrement
 
-func doSomething() {
-	counter = 1000
 }
