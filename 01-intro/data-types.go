52a89443ee7594e5e2b937053046ed2532d1a3a7
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-13 12:23:48 +0530

    variable declarations

--- 01-intro/data-types.go
+++ 01-intro/data-types.go
@@ -1,8 +1,8 @@
 package main
 
-import "fmt"
-
-var x int = 100
+import (
+	"fmt"
+)
 
 func main() {
 	/*
@@ -21,4 +21,61 @@ func main() {
 
 	msg := "Hello World!" /* Note => ":=" syntax is applicable only in a function */
 	fmt.Println(msg)
+
+	//fmt.Println(strconv.IntSize)
+
+	//Handling multiple variables
+	/*
+		var x int
+		var y int
+		var result int
+		var message string
+
+		x = 100
+		y = 200
+		result = x + y
+		message = "Result ="
+	*/
+
+	/*
+		var x int = 100
+		var y int = 200
+		var result int = x + y
+		var message string = "Result ="
+	*/
+
+	/*
+		var x, y, result int
+		var message string
+		x = 100
+		y = 200
+		result = x + y
+		message = "Result ="
+	*/
+	/*
+		var (
+			x, y, result int
+			message      string
+		)
+		x = 100
+		y = 200
+		result = x + y
+		message = "Result ="
+	*/
+	/*
+		var (
+			x, y, result int    = 100, 200, 0
+			message      string = "Result ="
+		)
+		result = x + y
+	*/
+
+	/*
+		x, y := 100, 200
+		message := "Result ="
+		result := x + y
+	*/
+	x, y, message := 100, 200, "Result ="
+	result := x + y
+	fmt.Println(message, result)
 }
