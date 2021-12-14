03a42170fa2099c5d69dcf50ab67ef185661ba58
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-14 12:09:50 +0530

    pointers assignment - solution

--- 05-pointers/program.go
+++ 05-pointers/program.go
@@ -12,10 +12,20 @@ func main() {
 
 	fmt.Println()
 	fmt.Println("Before incrementing, no = ", no)
-	increment(no)
+	increment(&no)
 	fmt.Println("After incrementing, no = ", no)
+
+	x, y := 10, 20
+	fmt.Println("before swapping ", x, y)
+	swap(&x, &y)
+	fmt.Println("after swapping ", x, y)
+
 }
 
-func increment(val int) {
-	val++
+func increment(val *int) {
+	*val++
+}
+
+func swap(no1Ptr, no2Ptr *int) {
+	*no1Ptr, *no2Ptr = *no2Ptr, *no1Ptr
 }
