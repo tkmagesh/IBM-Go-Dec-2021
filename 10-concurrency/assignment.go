0d615eeab0f068cede0024ea9822173d1ea8e4a3
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-15 16:07:23 +0530

    Create assignment.go

---
+++ 10-concurrency/assignment.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	data := []int{4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55}
+
+	/*
+		split the data into 2 equal sets
+		aggregate the data sets using the sum function (should happen concurrently)
+		collect the intermediate result
+		aggregate the intermediate result
+		print the final result
+	*/
+}
