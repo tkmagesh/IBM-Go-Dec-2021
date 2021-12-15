73556da5f282534113b399323699a3d1a27a94d1
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-15 15:38:43 +0530

    assignment - solution

--- 10-concurrency/demo-12.go
+++ 10-concurrency/demo-12.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	print("Hello")
-	print("World")
+	ch1, ch2 := make(chan string), make(chan string)
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go print("Hello", ch1, ch2, wg)
+	go print("World", ch2, ch1, wg)
+	ch1 <- "start"
+	wg.Wait()
 }
 
-func print(s string /* other arguments */) {
+func print(s string, in, out chan string, wg *sync.WaitGroup) {
 	for i := 0; i < 5; i++ {
+		<-in
 		time.Sleep(500 * time.Millisecond)
 		fmt.Println(s)
+		out <- "done"
 	}
+	wg.Done()
 }
 
 /*
