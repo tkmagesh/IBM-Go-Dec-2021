67c8ad411886494a9bb910dd419068320d32e376
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-15 10:06:42 +0530

    interfaces - part - 1

---
+++ 09-interfaces/program.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+type Rectangle struct {
+	Height float64
+	Width  float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+func main() {
+	c := Circle{Radius: 10}
+	//fmt.Println("Area = ", c.Area())
+	PrintArea(c)
+	PrintPerimeter(c)
+
+	r := Rectangle{Height: 10, Width: 20}
+	//fmt.Println("Area = ", r.Area())
+	PrintArea(r)
+	PrintPerimeter(r)
+}
+
+type ShapeWithArea interface {
+	Area() float64
+}
+
+func PrintArea(shapeWithArea ShapeWithArea) {
+	fmt.Println("Area = ", shapeWithArea.Area())
+}
+
+type ShapeWithPerimeter interface {
+	Perimeter() float64
+}
+
+func PrintPerimeter(shapeWithPerimeter ShapeWithPerimeter) {
+	fmt.Println("Perimeter = ", shapeWithPerimeter.Perimeter())
+}
